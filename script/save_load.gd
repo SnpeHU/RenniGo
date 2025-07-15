736a356a8559ab533aa4b733652e356b5f706b62
kouhoukei <48913150+YukariHu@users.noreply.github.com>
2025-07-16 02:29:48 +0900

    加密

--- script/save_load.gd
+++ script/save_load.gd
@@ -1,15 +1,18 @@
 extends Node
 
+var key = "Sample"
 
 func save()->void:
 	var config = ConfigFile.new()
 	config.set_value("Score_Data","high_score",Data.high)
-	config.save("user://player_data.cfg")
+	config.save_encrypted_pass("user://player_data.cfg",key)
 	
 	print("saved!")
 
 func load()->void:
 	var config = ConfigFile.new()
-	var result =  config.load("user://player_data.cfg")
+	var result =  config.load_encrypted_pass("user://player_data.cfg",key)
 	if result == OK:
 		Data.high = config.get_value("Score_Data","high_score")
+	else:
+		printerr("error on load")
